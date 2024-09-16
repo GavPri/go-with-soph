a2c00b64b86f18b00f799373f62eb034a8cccb33
德华 刘 <gavinprior5@gmail.com>
2024-09-16 17:35:30 +0000

    add useState to chnage icon for hamburger menu

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,13 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 
 const NavigationBar = () => {
+  const [openHamburgerMenu, setOpenHamburgerMenu] = useState(false);
+  const handleHamburgerMenu = () => {
+    setOpenHamburgerMenu(!openHamburgerMenu);
+  };
   const HamburgerMenuIcon = () => {
     return (
-      <div className="h-4 w-6 relative bg-body">
-        <span className="absolute top-0 bg-accentPrimary h-[3px] w-full"></span>
-        <span className="absolute top-[50%] bg-accentPrimary h-[3px] w-full"></span>
-        <span className="absolute top-[50%] bg-accentPrimary h-[3px] w-full"></span>
-        <span className="absolute top-[100%] bg-accentPrimary h-[3px] w-full"></span>
+      <div
+        className="h-4 w-6 relative bg-body cursor-pointer transition-all"
+        onClick={handleHamburgerMenu}
+      >
+        {/* Top line (shrinks when menu is open) */}
+        <span
+          className={`absolute bg-accentPrimary h-[3px] w-full top-0 ${
+            openHamburgerMenu ? "w-0" : "w-full"
+          }`}
+        ></span>
+
+        {/* Middle line (rotates to 45 degrees) */}
+        <span
+          className={`absolute bg-accentPrimary h-[3px] w-full top-[50%] ${
+            openHamburgerMenu ? "transition-transform" : "transition-transform"
+          }`}
+        ></span>
+
+        {/* Bottom line (rotates to -45 degrees) */}
+        <span
+          className={`absolute bg-accentPrimary h-[3px] w-full top-[50%] ${
+            openHamburgerMenu ? "-rotate-45" : "rotate-0"
+          }`}
+        ></span>
+
+        {/* Bottom-most line (shrinks when menu is open) */}
+        <span
+          className={`absolute bg-accentPrimary h-[3px] w-full top-[100%] ${
+            openHamburgerMenu ? "w-0" : "w-full"
+          }`}
+        ></span>
       </div>
     );
   };
