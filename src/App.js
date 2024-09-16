3c56c059b11b1bec4defd8b9eff11a5a92156dd6
德华 刘 <gavinprior5@gmail.com>
2024-09-16 19:05:01 +0000

    move useEffect and useState for theme change
    create a button for theme change

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+
 import NavigationBar from "./components/NavigationBar";
 import {
   createBrowserRouter,
@@ -25,31 +25,6 @@ function App() {
     )
   );
 
-  // * useEffect to handle theme changes
-  useEffect(() => {
-    // If the user has selected an theme, get that theme
-    const selectedTheme = localStorage.getItem("theme");
-
-    if (selectedTheme) {
-      document.body.classList.add(selectedTheme);
-    } else if (window.matchMedia("(prefers-color-scheme: dark)")) {
-      document.body.classList.add("dark");
-    } else {
-      document.body.classList.add("light");
-    }
-  }, []);
-
-  const [selectedColorScheme, setselectedColorScheme] = useState("light");
-
-  const handleColorSchemeChange = () => {
-    const newTheme = selectedColorScheme === "light" ? "dark" : "light";
-
-    document.body.classList.remove(selectedColorScheme);
-
-    document.body.classList.add(newTheme);
-
-    setselectedColorScheme(newTheme);
-  };
   return (
     <div className="bg-body">
       <RouterProvider router={router} />
--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,7 +1,34 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { LuSun } from "react-icons/lu";
 
 const NavigationBar = () => {
   const [openHamburgerMenu, setOpenHamburgerMenu] = useState(false);
+  // * useEffect to handle theme changes
+  useEffect(() => {
+    // If the user has selected an theme, get that theme
+    const selectedTheme = localStorage.getItem("theme");
+
+    if (selectedTheme) {
+      document.body.classList.add(selectedTheme);
+    } else if (window.matchMedia("(prefers-color-scheme: dark)")) {
+      document.body.classList.add("dark");
+    } else {
+      document.body.classList.add("light");
+    }
+  }, []);
+
+  const [selectedColorScheme, setselectedColorScheme] = useState("light");
+
+  const handleColorSchemeChange = () => {
+    const newTheme = selectedColorScheme === "light" ? "dark" : "light";
+
+    document.body.classList.remove(selectedColorScheme);
+
+    document.body.classList.add(newTheme);
+
+    setselectedColorScheme(newTheme);
+  };
   const handleHamburgerMenu = () => {
     setOpenHamburgerMenu(!openHamburgerMenu);
   };
@@ -49,7 +76,33 @@ const NavigationBar = () => {
             ? "max-h-[calc(100%-96px)] bg-body"
             : "max-h-0 bg-transparent"
         } w-screen bg-body absolute left-0 top-24 transition-all duration-500 ease-in-out`}
-      ></div>
+      >
+        {openHamburgerMenu && (
+          <div
+            className="w-full flex flex-col gap-2 items-center justify-between"
+            onClick={handleHamburgerMenu}
+          >
+            <Link className="p-4" to="/">
+              Home
+            </Link>
+            <Link className="p-4" to="/about">
+              About
+            </Link>
+            <Link className="p-4" to="/contact">
+              Contact
+            </Link>
+            <Link className="p-4" to="/destinations">
+              Destinations
+            </Link>
+            <button
+              className="rounded-full bg-text h-6 w-fit text-accentPrimary flex justify-center items-center p-4"
+              onClick={handleColorSchemeChange}
+            >
+              Change Theme <LuSun className="pl-1" />
+            </button>
+          </div>
+        )}
+      </div>
     );
   };
 
