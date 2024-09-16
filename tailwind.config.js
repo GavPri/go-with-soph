339ad115b338e7dd9c1ca13a4cb4e8e33febb81d
德华 刘 <gavinprior5@gmail.com>
2024-09-16 14:52:56 +0000

    add rgba values with variants to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        body: "rgba(var(--body)",
+        text: "rgba(var(--text)",
+        accentPrimary: "rgba(var(--accent-primary)",
+        accentSecondary: "rgba(var(--accent-secondary)"
+      },
+    },
   },
   plugins: [],
 };
