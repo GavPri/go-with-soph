d5b9c66570b2bae5ad1254cfc5d1584e755e311f
德华 刘 <gavinprior5@gmail.com>
2024-09-16 15:44:39 +0000

    add useEffect and useState to handle theme changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
+  // * useEffect to handle theme changes
+  useEffect(() => {
+    // If the user has selected an theme, get that theme
+    const selectedTheme = localStorage.getItem("theme");
+
+    if (selectedTheme) {
+      document.body.classList.add(selectedTheme);
+    } else if (window.matchMedia("(prefers-color-scheme: dark)")) {
+      document.body.classList.add("dark");
+    } else {
+      document.body.classList.add("light");
+    }
+  }, []);
+
+  const [selectedColorScheme, setselectedColorScheme] = useState("light");
+
+  const handleColorSchemeChange = () => {
+    const newTheme = selectedColorScheme === "light" ? "dark" : "light";
+
+    document.body.classList.remove(selectedColorScheme);
+
+    document.body.classList.add(newTheme);
+
+    setselectedColorScheme(newTheme);
+  };
   return (
-    <div className="light w-screen h-screen">
-      <div className="h-full w-full bg-body">
-        <p>Hello, world!</p>
-        <button className="bg-accentPrimary">Click here!</button>
-      </div>
+    <div className="bg-body">
+      <h1 className="text-text text-3xl font-bold font-mono">GoWithSoph!</h1>
+      <button
+        className="bg-accentPrimary h-10 w-24"
+        onClick={handleColorSchemeChange}
+      ></button>
     </div>
   );
 }
