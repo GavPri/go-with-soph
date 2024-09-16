620f408c83e8b09536f5ed8c2bdc006592e4807f
德华 刘 <gavinprior5@gmail.com>
2024-09-16 16:09:59 +0000

    style initial hamburger menu for mobile devices

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,7 +1,23 @@
 import React from "react";
 
 const NavigationBar = () => {
-  return <div>NavigationBar</div>;
+  const HamburgerMenuIcon = () => {
+    return (
+      <div className="h-4 w-6 relative bg-body">
+        <span className="absolute top-0 bg-accentPrimary h-[3px] w-full"></span>
+        <span className="absolute top-[50%] bg-accentPrimary h-[3px] w-full"></span>
+        <span className="absolute top-[50%] bg-accentPrimary h-[3px] w-full"></span>
+        <span className="absolute top-[100%] bg-accentPrimary h-[3px] w-full"></span>
+      </div>
+    );
+  };
+
+  return (
+    <div className="w-screen h-24 bg-body flex justify-between items-center p-6">
+      <h1 className="text-text font-bold">GoWithSoph</h1>
+      <HamburgerMenuIcon />
+    </div>
+  );
 };
 
 export default NavigationBar;
