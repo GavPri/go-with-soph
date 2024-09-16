5fa33ff3fae597376d3eee2aa900ac4a46994aff
德华 刘 <gavinprior5@gmail.com>
2024-09-16 20:37:56 +0000

    style header for latest-post

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,8 +7,15 @@ const Home = () => {
       <img
         src="/images/nevadaFalls.JPG"
         alt="Sophie at the top of nevada falls"
-        className="max-w-screen min-h-screen object-cover"
+        className="max-w-screen min-h-screen object-cover mb-10"
       />
+      {/* Latest from the blog */}
+      <div className="w-screen h-screen bg-body font-qs text-text">
+        <h2 className="text-2xl text-center pb-4 capitalize border-b-2 border-accentPrimary mx-auto w-fit">
+          Latest from the blog
+        </h2>
+      </div>
+      {/* Most popular from the blog */}
     </div>
   );
 };
