748647feaf485c6a3f26d349ead9f7d7ae11fef3
德华 刘 <gavinprior5@gmail.com>
2024-09-17 11:07:00 +0000

    create the signup function in AuthControllers

--- server/controllers/AuthContollers.js
+++ server/controllers/AuthContollers.js
@@ -1,3 +1,24 @@
 const User = require("../models/UserModel");
 const { createSecretToken } = require("../util/SecretToken");
 const bcrypt = require("bcryptjs");
+
+module.exports.Signup = async (req, res, next) => {
+  try {
+    const { email, username, password, role } = req.body;
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.json({ message: "User already exists" });
+    }
+    const user = await User.create({ email, username, password, role });
+    const token = createSecretToken(user_id);
+    res.cookie("token", token, {
+      withCredentials: true,
+      httpOnly: false,
+    });
+    res
+      .status(201)
+      .json({ message: "Signed In Successfully", success: true, user });
+  } catch (error) {
+    console.error(error);
+  }
+};
