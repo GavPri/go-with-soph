afdce93685f8f39d1dfb62fc8489d280ba203f2b
德华 刘 <gavinprior5@gmail.com>
2024-09-17 10:36:24 +0000

    create first draft of user schema

--- server/Models/UserModel.js
+++ server/Models/UserModel.js
@@ -1,13 +1,31 @@
 // * requiremnts for user model
 
-const mongoose = require('mongoose');
-const bcrypt = require('bcrypt')
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
-// * user schema 
+// * user schema
 
-const userSchema = new mongoose.Schema({
-    email:{},
-    username:{},
-    password:{},
-    createdAt:{}
-})
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: [true, "Your email address is required"],
+      unique: true,
+      match: [/.+\@.+\..+/, "Please enter a valid email address"],
+    },
+    username: {
+      type: String,
+      required: [true, "Your username is required"],
+    },
+    password: {
+      type: String,
+      required: [true, "Your password is required"],
+    },
+    role: {
+      type: String,
+      enum: ["admin", "user"], // Define user roles
+      default: "user", // Default to regular user
+    },
+  },
+  { timestamps: true }
+);
