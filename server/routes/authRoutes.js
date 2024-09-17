0a04c4c69d1c52666fb32ae217911f1ea0dfd32d
德华 刘 <gavinprior5@gmail.com>
2024-09-17 15:24:41 +0000

    create authRoutes & Authcontroller

---
+++ server/routes/authRoutes.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+const cors = require("cors");
+
+// * middleware
+router.use(
+  cors({
+    credentials: true,
+    origin: "*",
+  })
+);
+
+router.post('/register', registerUser)
