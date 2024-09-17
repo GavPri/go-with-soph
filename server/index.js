6933c53229a664c252be6418e7825c62cef2aec9
德华 刘 <gavinprior5@gmail.com>
2024-09-17 10:20:40 +0000

    configure the connection to mongoDB

--- server/index.js
+++ server/index.js
@@ -1,8 +1,25 @@
 const express = require("express");
-
+const mongoose = require("mongoose");
+const cors = require("cors");
 const app = express();
-const PORT = 5000;
+require("dotenv").config();
+const { MONGODB_URL, PORT } = process.env;
+
+mongoose
+  .connect(MONGODB_URL)
+  .then(() => console.log("MONGODB is connected."))
+  .catch((error) => console.log(error));
 
 app.listen(PORT, () => {
   console.log(`Server is listenting on port ${PORT}`);
 });
+
+app.use(
+  cors({
+    origin: ["http://local.host:4000"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
+app.use(express.json());
